39c7a4a284d34b197caf1611593976b03f2fae1e
OKOJ <aajmd@hotmail.com>
2019-02-19 22:46:02 -0700

    adding pic and editing html file

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var compGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guestGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guestGuessesLower = guestGuesses.toLowerCase();
+
+
+
+
+//compering guesses
+ 
