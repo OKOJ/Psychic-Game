4d44b4cbcea584a2a27a02b08f581c04db50a05c
OKOJ <aajmd@hotmail.com>
2019-02-23 08:56:51 -0700

    writing JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,65 @@
 //computer chooses a random letter (a-z);
-var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+console.log(computerGuess);
 
 //player chooses a random 9 letters (a-z);
-var playerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
-var guesses = [];
+var guessesLeft = 9;
+var playerGuesses = [];
 
+//References to the places in the HTML <span>
+var playerWinsText = document.getElementById("wins-text");
+var playerLossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var playerGuessesText = document.getElementById("yourGuesses-text");
 
-//if one of the player's choices are equal to the computer's choice, player wins;
-//if after 9 tries one of the player's choices are not equal to the computer's choice, player lost;
-//keeping scores
-//Guesses Left: starts with 9 and decreases by 1 with every player's choice untill player wins or it is 0;
-//Your Guesses So Far: display the letters than player typed in untill player wins or lose;
-//Wins: when player wins, increase the Wins by 1 and start the game again without refreshing the page; 
-//Losses: when player lose, increase the Losses by 1 and start the game again without refreshing the page;
+document.onkeyup = function (event) {
+
+    guessesLeft--;
+    var playerGuess = event.key;
+    var playerGuessLower = playerGuess.toLowerCase();
+    // add letter to playerGuesses array
+    playerGuesses.push(playerGuess);
+    // show letter on page
+    playerGuessesText.textContent = playerGuesses;
+    // change guessesLeftText to guessesLeft
+    guessesLeftText.textContent = guessesLeft;
+    //set the guesses to 9 tries
+    function resetGame() {
+
+        guessesLeft = 9;
+        playerGuesses = [];
+
+        //References to the places in the HTML <span>
+
+        guessesLeftText = document.getElementById("guessesLeft-text");
+        playerGuessesText = document.getElementById("yourGuesses-text");
+    };
+
+    //you win 
+    if (playerGuessLower === computerGuess) {
+        //if you win increament wins
+        wins++;
+        // change playerWinsText
+        playerWinsText.textContent = wins;
+
+    };
+
+    // you lose
+    if (guessesLeft === 0 && playerGuessLower !== computerGuess) {
+        //if you lose increament losses
+        losses++;
+        // change playerLossesText
+        playerLossesText.textContent = losses;
+
+    };
+
+    //reset
+    if (guessesLeft === 0) {
+
+        resetGame();
+    };
+}
